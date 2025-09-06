c108f05d922d547c554f454a2f9a8ca32bcb823f
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-09-06 19:07:16 +0700

    Add basic entity

---
+++ src/main/java/dev/tdminhnhat/entity/AbstractEntityProperty.java
@@ -0,0 +1,50 @@
+package dev.tdminhnhat.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Version;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.ZonedDateTime;
+
+@MappedSuperclass
+@Getter @Setter
+@NoArgsConstructor
+public abstract class AbstractEntityProperty {
+
+    @Column(name = "delete_flag", nullable = false, columnDefinition = "BOOLEAN DEFAULT FALSE")
+    private Boolean deleteFlag;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private ZonedDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private ZonedDateTime updatedAt;
+
+    @Column(name = "created_by", length = 100, nullable = false, updatable = false)
+    private String createdBy;
+
+    @Column(name = "updated_by", length = 100, nullable = false)
+    private String updatedBy;
+
+    @Version
+    @Column(name = "version", nullable = false)
+    private Integer version;
+
+    public AbstractEntityProperty(String createdBy, String updatedBy) {
+        this.createdBy = createdBy;
+        this.updatedBy = updatedBy;
+    }
+
+    public void onPrePersist() {
+        createdAt = ZonedDateTime.now();
+        deleteFlag = false;
+    }
+
+    public void onPreUpdate() {
+        updatedAt = ZonedDateTime.now();
+    }
+}
---
+++ src/main/java/dev/tdminhnhat/entity/topics/basic/Person.java
@@ -0,0 +1,76 @@
+package dev.tdminhnhat.entity.topics.basic;
+
+import dev.tdminhnhat.entity.AbstractEntityProperty;
+import dev.tdminhnhat.entity.topics.basic.enums.PersonStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity @Table(name = "students")
+@Getter @Setter
+@NoArgsConstructor @RequiredArgsConstructor
+public class Person extends AbstractEntityProperty {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "first_name", length = 50, nullable = false)
+    @NonNull
+    private String firstName;
+
+    @Column(name = "last_name", length = 50, nullable = false)
+    @NonNull
+    private String lastName;
+
+    @Column(name = "sex", nullable = false)
+    @NonNull
+    private Boolean sex;
+
+    @Column(name = "birth_date", nullable = false)
+    @NonNull
+    private LocalDate birthDate;
+
+    @Column(name = "address", length = 200)
+    private String address;
+
+    @Column(name = "user_name", length = 50, nullable = false, unique = true)
+    @NonNull
+    private String username;
+
+    @Column(name = "email", length = 200, nullable = false, unique = true)
+    @NonNull
+    private String email;
+
+    @Column(name = "password", length = 100, nullable = false)
+    @NonNull
+    private String password;
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "status", nullable = false, columnDefinition = "DEFAULT 1")
+    private PersonStatus status;
+
+    public Person(String createdBy, String updatedBy, @NonNull String firstName, @NonNull String lastName, @NonNull Boolean sex, @NonNull LocalDate birthDate, String address, @NonNull String username, @NonNull String email, @NonNull String password) {
+        super(createdBy, updatedBy);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.sex = sex;
+        this.birthDate = birthDate;
+        this.address = address;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    @PrePersist
+    public void onPrePersist() {
+        super.onPrePersist();
+        this.status = PersonStatus.ACTIVE;
+    }
+
+    @PreUpdate
+    public void onPreUpdate() {
+        super.onPreUpdate();
+    }
+}
